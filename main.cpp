1ec3e086e7d082a96444e1bbba3a173b53391c7e
PodySIN <daniil2007xboctob@gmail.com>
2026-03-17 10:52:25 +0300

    Upgrade makefile and add gitignore

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include "top-it-vector.hpp"
 
 int main()
 {}
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -1,6 +1,8 @@
 #ifndef TOP_IT_VECTOR_HPP
 #define TOP_IT_VECTOR_HPP
 
+#include <cstddef>
+
 template< class T >
 struct Vector{
   T* data;
