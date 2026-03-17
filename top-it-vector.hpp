acbe3e38a0a87b4616476a8ac1193a0ead059ba0
PodySIN <daniil2007xboctob@gmail.com>
2026-03-17 11:09:04 +0300

    Add basic test framework and small vector changes

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -1,13 +1,31 @@
 #ifndef TOP_IT_VECTOR_HPP
 #define TOP_IT_VECTOR_HPP
-
 #include <cstddef>
 
+namespace topit {
+  template< class T >
+  class Vector {
+    public:
+      ~Vector();
+      Vector();
+
+    private:
+      T* data_;
+      size_t size_, capacity_;
+  };
+}
+
 template< class T >
-struct Vector{
-  T* data;
-  size_t size, capacity;
-};
+topit::Vector< T >::~Vector()
+{
+  delete[] data_;
+}
+
+template< class T >
+topit::Vector< T >::Vector():
+  data_(nullptr),
+  size_(0),
+  capacity_(0)
+{}
 
 #endif
-
