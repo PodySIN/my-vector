8349739399fecb3a375aefd10e7908611b808046
PodySIN <daniil2007xboctob@gmail.com>
2026-03-24 11:39:18 +0300

    Add copy and move constructor, add copy and move operator, and test for copy and move constructor(empty and non empty

--- main.cpp
+++ main.cpp
@@ -28,6 +28,20 @@ bool testGetSize()
   return start == 0 && v.getSize() == 4;
 }
 
+bool testPopBack()
+{
+  topit::Vector< int > v;
+  v.pushBack(5);
+  v.pushBack(5);
+  v.pushBack(5);
+  v.pushBack(5);
+  size_t start = v.getSize();
+  v.popBack();
+  v.popBack();
+  return start == 4 && v.getSize() == 2;
+
+}
+
 bool testElementInboundConstAccess()
 {
   topit::Vector< int > v;
@@ -80,6 +94,51 @@ bool testElementOutboundAccess()
   }
 }
 
+bool testEqual()
+{
+  topit::Vector< int > l;
+  l.pushBack(1);
+  l.pushBack(2);
+  l.pushBack(3);
+  topit::Vector< int > r;
+  r.pushBack(1);
+  r.pushBack(2);
+  r.pushBack(3);
+  return l == r;
+}
+
+bool testCopyConstructorForEmpty()
+{
+  topit::Vector< int > v;
+  topit::Vector< int > yav{v};
+  return v == yav;
+}
+
+bool testCopyConstructorForNonEmpty()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  v.pushBack(2);
+  topit::Vector< int > yav{v};
+  return v == yav;
+}
+
+bool testMoveConstructorForEmpty()
+{
+  topit::Vector< int > v;
+  topit::Vector< int > yav{std::move(v)};
+  return v == yav;
+}
+
+bool testMoveConstructorForNonEmpty()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  v.pushBack(2);
+  topit::Vector< int > yav{std::move(v)};
+  return v == yav;
+}
+
 int main()
 {
   using test_t = std::pair< const char*, bool(*)() >;
@@ -87,10 +146,16 @@ int main()
     { "Empty vector", testEmptyVector },
     { "Push back element", testPushBack },
     { "Get size", testGetSize },
+    { "Pop Back", testPopBack },
     { "Inbound access", testElementInboundAccess },
     { "Outbound access", testElementOutboundAccess },
     { "Inbound const access", testElementInboundConstAccess },
-    { "Outbound const access", testElementOutboundConstAccess }
+    { "Outbound const access", testElementOutboundConstAccess },
+    { "Equal of 2 vectors", testEqual },
+    { "Test copy constructor for empty", testCopyConstructorForEmpty },
+    { "Test copy constructor for non empty", testCopyConstructorForNonEmpty },
+    { "Test move constructor for empty", testMoveConstructorForEmpty },
+    { "Test move constructor for non empty", testMoveConstructorForNonEmpty }
   };
   const size_t count = sizeof(tests) / sizeof(test_t);
   std::cout << std::boolalpha;
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -9,17 +9,20 @@ namespace topit {
       ~Vector();
       Vector();
       Vector(const Vector&);
-      Vector(Vector&&);
+      Vector(size_t size, const T& init);
+      Vector(Vector&&) noexcept;
       Vector& operator=(const Vector&);
       Vector& operator=(Vector&&);
+
       T& operator[](size_t index) noexcept;
       const T& operator[](size_t index) const noexcept;
-      
+
       bool isEmpty() const noexcept;
       size_t getSize() const noexcept;
       size_t getCapacity() const noexcept;
 
       void reallocate(size_t capacity);
+      void swap(Vector< T >& rhs) noexcept;
       void pushBack(const T& v);
       void popBack();
       void insert(size_t pos, const T* v);
@@ -27,9 +30,34 @@ namespace topit {
       T& at(size_t index);
       const T& at(size_t index) const;
     private:
+      explicit Vector(size_t size);
       T* data_;
       size_t size_, capacity_;
   };
+
+  template< class T >
+  bool operator==(const Vector< T >& lhs, const Vector< T >& rhs);
+}
+
+template< class T >
+topit::Vector< T >::Vector(size_t size):
+  data_(size ? new T[size] : nullptr),
+  size_(size),
+  capacity_(size)
+{}
+
+template< class T >
+topit::Vector< T >::Vector(size_t size, const T& init):
+  Vector(size)
+{
+  for (size_t i = 0; i < size; i++) {
+    try {
+      data_[i] = init;
+    } catch (...) {
+      delete[] data_;
+      throw;
+    }
+  }
 }
 
 template< class T >
@@ -44,6 +72,51 @@ topit::Vector< T >::Vector():
   size_(0),
   capacity_(0)
 {}
+
+template< class T >
+topit::Vector< T >::Vector(const Vector< T >& rhs):
+  Vector(rhs.getSize())
+{
+  for (size_t i = 0; i < rhs.getSize(); i++) {
+    data_[i] = rhs[i];
+  }
+}
+
+template< class T >
+topit::Vector< T >::Vector(Vector< T >&& rhs) noexcept:
+  data_(rhs.data_),
+  size_(rhs.size_),
+  capacity_(rhs.capacity_)
+{
+  rhs.data_ = nullptr;
+}
+
+template< class T >
+void topit::Vector< T >::swap(Vector< T >& rhs) noexcept
+{
+  std::swap(data_, rhs.data_);
+  std::swap(size_, rhs.size_);
+  std::swap(capacity_, rhs.capacity_);
+}
+
+template< class T >
+topit::Vector< T >& topit::Vector< T >::operator=(const Vector< T >& rhs)
+{
+  Vector< T > cpy{rhs};
+  swap(cpy);
+  return *this;
+}
+
+template< class T >
+topit::Vector< T >& topit::Vector< T >::operator=(Vector< T >&& rhs)
+{
+  if (this == std::addressof(rhs)) {
+    return *this;
+  }
+  Vector< T > cpy{std::move(rhs)};
+  swap(cpy);
+  return *this;
+}
 
 template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept
@@ -67,7 +140,7 @@ template< class T >
 void topit::Vector< T >::reallocate(size_t capacity)
 {
   T* data = new T[capacity];
-  for (size_t i =0; i < size_; i++) {
+  for (size_t i = 0; i < size_; i++) {
     data[i] = std::move(data_[i]);
   }
   delete[] data_;
@@ -110,19 +183,27 @@ const T& topit::Vector< T >::operator[](size_t index) const noexcept
 template< class T >
 T& topit::Vector< T >::at(size_t index)
 {
-  if (index >= size_) {
-    throw std::out_of_range("Index too big");
-  }
-  return data_[index];
+  const Vector< T >* cthis = this;
+  return const_cast< T& >(cthis->at(index));
 }
 
 template< class T >
 const T& topit::Vector< T >::at(size_t index) const
 {
   if (index >= size_) {
-    throw std::out_of_range("Index too big");
+    throw std::out_of_range("Bad index");
   }
   return data_[index];
+}
+
+template< class T >
+bool topit::operator==(const Vector< T >& lhs, const Vector< T >& rhs)
+{
+  bool is_equal = lhs.getSize() == rhs.getSize();
+  for (size_t i = 0; (i < lhs.getSize()) && is_equal; i++) {
+    is_equal = is_equal && (lhs[i] == rhs[i]);
+  }
+  return is_equal;
 }
 
 #endif
